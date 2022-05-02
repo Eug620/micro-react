3b0d41b3d8710acd9007fa90ed24870e2b483887
eug620 <yyh3531@163.com>
2022-05-02 15:19:04 +0800

    what fuck

--- src/views/Todolist/index.tsx
+++ src/views/Todolist/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Card } from "@arco-design/web-react";
 
-export default class Todolist extends React.Component {
+class Todolist extends React.Component {
   render() {
     return <Card style={{ height: "100%" }}>Todolist</Card>;
   }
 }
+
+export default Todolist
